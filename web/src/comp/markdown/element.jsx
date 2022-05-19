46c0b2a3770a50809ee6dcdc5e1548833b4a8b6d
xiaohu <xiaohu@lucky-byte.com>
2022-05-19 19:05:32 +0800

    Markdown 图片支持放大显示

--- web/src/comp/markdown/element.jsx
+++ web/src/comp/markdown/element.jsx
@@ -1,17 +1,21 @@
 import { createElement } from 'react';
 import Image from './image';
 
-// 可以在 Markdown 中使用下列的组件
+// 可以在 Markdown 中使用下列 React 组件
 const customElements = {
   'Image': Image,
 }
 
-const allowTags = [
-  'p', 'ul', 'ol', 'li', 'span', 'em', 'strong', 'code', 'sup', 'sub', 'del', 'br',
-  'div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'footer', 'section', 'input',
-  'summary', 'details'
+// 只允许渲染下列 HTML 元素
+const allowHtmlElements = [
+  'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'div', 'br',
+  'main', 'header', 'footer', 'section', 'summary', 'details',
+  'p', 'span', 'ul', 'ol', 'li',
+  'em', 'strong', 'del', 'i', 'b', 'u', 'code', 'sup', 'sub',
+  'input',
 ]
 
+// 这个函数用于控制最终可以渲染的元素，包括 React 组件和 HTML 元素
 function createCustomElement(tag, props, ...children) {
   if (typeof tag === 'string') {
     // 大写字母开头认为是组件
@@ -21,22 +25,25 @@ function createCustomElement(tag, props, ...children) {
         return createElement(comp, props, children)
       }
       if (process.env.NODE_ENV === 'development') {
-        console.warn('不支持 Markdown 组件 ', tag, props);
+        console.log('不支持 Markdown 组件 ', tag, props);
       }
       return <></>;
     } else {
-      if (allowTags.indexOf(tag) < 0) {
+      if (allowHtmlElements.indexOf(tag) < 0) {
         if (process.env.NODE_ENV === 'development') {
-          console.log('不支持 Markdown 标签 ', tag, props);
+          console.log('不支持 HTML 元素 ', tag, props);
         }
         return <></>;
       }
+      // 只允许 Checkbox 类型的输入
       if (tag === 'input') {
         if (props?.type === 'checkbox') {
           return createElement(tag, props, ...children);
-        } else {
-          return <></>;
         }
+        if (process.env.NODE_ENV === 'development') {
+          console.log('不支持 HTML 元素 ', tag, props);
+        }
+        return <></>;
       }
     }
   }
--- web/src/comp/markdown/image.jsx
+++ web/src/comp/markdown/image.jsx
@@ -1,9 +1,26 @@
+import { useTheme } from '@mui/material/styles';
+import Zoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css';
 
-// 图片
+// 图片，可以点击放大
 export default function Image(props) {
+  const theme = useTheme();
+
+  let overlayBgColorStart = 'rgba(255, 255, 255, 0)';
+  let overlayBgColorEnd = 'rgba(255, 255, 255, 0.9)';
+
+  if (theme.palette.mode === 'dark') {
+    overlayBgColorStart = 'rgba(0, 0, 0, 0)';
+    overlayBgColorEnd = 'rgba(0, 0, 0, 0.9)';
+  }
+
   return (
-    <img alt={props.alt || '图片'} src={props.src} style={{
-      objectFit: 'contain', maxWidth: '100%',
-    }} />
+    <Zoom wrapElement='span' closeText='缩小' openText='放大'
+      overlayBgColorStart={overlayBgColorStart}
+      overlayBgColorEnd={overlayBgColorEnd}>
+      <img alt={props.alt || '图片'} src={props.src} style={{
+        objectFit: 'contain', maxWidth: '100%',
+      }} />
+    </Zoom>
   )
 }
